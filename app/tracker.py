533c320c05530e906eebb400cac8766df97b9a73
nextpassword99 <inderscop@gmail.com>
2025-03-14 12:48:40 -0500

    refactor(tracker): Refactorizar descarga del modelo para usar Downloader

--- app/tracker.py
+++ app/tracker.py
@@ -2,15 +2,17 @@ import numpy as np
 import mediapipe as mp
 from mediapipe.tasks import python as py
 from mediapipe.tasks.python import vision as vs
-import os
-import requests
+
+from app.downloader import Downloader
 
 
 class Tracker:
-    def __init__(self, model_path='data/models/efficientdet_lite0.tflite'):
+    def __init__(self):
         self.detector = None
-        self.model_path = model_path
-        self.download_model()
+        self.model_url = 'https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/int8/1/efficientdet_lite0.tflite'
+        self.model_path = None
+        self.downloader = Downloader()
+        self.model_path = self.downloader.download_model(self.model_url)
         self.configure_options()
 
     def configure_options(self):
@@ -29,19 +31,3 @@ class Tracker:
 
         result = self.detector.detect(mp_image)
         return result
-
-    def download_model(self):
-        model_url = "https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/int8/1/efficientdet_lite0.tflite"
-        if not os.path.exists(self.model_path):
-            print("Descargando modelo...")
-            try:
-                response = requests.get(model_url)
-                response.raise_for_status()
-
-                with open(self.model_path, "wb") as f:
-                    f.write(response.content)
-
-                print(f"Modelo descargado: {self.model_path}")
-
-            except requests.exceptions.RequestException as e:
-                print(f"Error al descargar el modelo: {e}")
